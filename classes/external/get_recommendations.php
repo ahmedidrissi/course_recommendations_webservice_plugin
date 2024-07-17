54ae8a99d2439479886d2ca6d94a2b691085c65c
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-07-17 15:34:22 +0100

    connect the ws with fast api

--- classes/external/get_recommendations.php
+++ classes/external/get_recommendations.php
@@ -23,7 +23,6 @@ class get_recommendations extends external_api
             'user_id' => new external_value(PARAM_INT, 'the id of the user'),
             'institution' => new external_value(PARAM_TEXT, 'the institution of the user'),
             'function' => new external_value(PARAM_TEXT, 'the function of the user'),
-            'lang' => new external_value(PARAM_TEXT, 'the language of the user'),
             'category_id' => new external_value(PARAM_INT, 'the id of the parent category'),
         ]);
     }
@@ -51,37 +50,25 @@ class get_recommendations extends external_api
      * @param int $user_id the id of the user
      * @param string $institution the institution of the user
      * @param string $function the function of the user
-     * @param string $lang the language of the user
+     * @param int $category_id the id of the parent category
      * @return array of course recommendations
      */
     public static function execute(
         $user_id,
         $institution,
         $function,
-        $lang,
         $category_id
-    )
-    {
-        global $USER, $DB;
+    ) {
+        global $USER;
 
         // Validate the user_id
         if ($user_id != $USER->id) {
             throw new \moodle_exception('invalid_user_id', 'local_course_recommendations_ws');
         }
 
-        // Validate the institution
-        if ($institution == '') {
-            throw new \moodle_exception('invalid_institution', 'local_course_recommendations_ws');
-        }
-
-        // Validate the function
-        if ($function == '') {
-            throw new \moodle_exception('invalid_function', 'local_course_recommendations_ws');
-        }
-
-        // Validate the lang
-        if ($lang != 'en' && $lang != 'fr') {
-            throw new \moodle_exception('invalid_lang', 'local_course_recommendations_ws');
+        // Validate other parameters
+        if ($institution == '' || $function == '') {
+            throw new \moodle_exception('invalid_parameters', 'local_course_recommendations_ws');
         }
 
         // Get the user context and validate it
@@ -91,46 +78,145 @@ class get_recommendations extends external_api
 
         // Create an empty array to store the recommendations
         $recommendations = array();
+        $items = self::get_categories_and_courses($category_id);
 
-        // Check if the category_id is set
-        if($category_id) {
+        // Get the recommendations
+        $recommendations = self::send_request($user_id, $institution, $function, $items);
+
+        // TODO: Validate the recommendations using context
+
+        return $recommendations;
+    }
+
+    /**
+     * Helper function to get categories and courses
+     * @param int $category_id the id of the parent category
+     * @return array of categories and courses
+     */
+    private static function get_categories_and_courses($category_id)
+    {
+        $items = [
+            'parent_category' => [],
+            'categories' => [],
+            'subcategories' => [],
+            'courses' => []
+        ];
+
+        if ($category_id != 0) {
             // Get the top category
             $category = core_course_category::get($category_id);
+
+            // Add the top category to the items array
+            $items['parent_category'] = [
+                'id' => $category->id,
+                'name' => $category->get_formatted_name(),
+                'coursecount' => $category->get_courses_count(),
+                'parent' => $category->get_parent_coursecat()->id,
+                'depth' => $category->depth,
+                'path' => $category->path
+            ];
 
             // Get the categories under the top category
             $categories = $category->get_children();
 
             foreach ($categories as $category) {
+                // Add the category to the items array
+                $items['categories'][] = [
+                    'id' => $category->id,
+                    'name' => $category->get_formatted_name(),
+                    'coursecount' => $category->get_courses_count(),
+                    'parent' => $category->get_parent_coursecat()->id,
+                    'depth' => $category->depth,
+                    'path' => $category->path
+                ];
+
                 // Get the subcategories of the category
                 $subcategories = $category->get_children();
 
                 foreach ($subcategories as $subcategory) {
+                    // Add the subcategory to the items array
+                    $items['subcategories'][] = [
+                        'id' => $subcategory->id,
+                        'name' => $subcategory->get_formatted_name(),
+                        'coursecount' => $subcategory->get_courses_count(),
+                        'parent' => $subcategory->get_parent_coursecat()->id,
+                        'depth' => $subcategory->depth,
+                        'path' => $subcategory->path
+                    ];
+
                     // Get the courses under the subcategory
                     $courses = $subcategory->get_courses();
-
-                    // Get top 3 courses
-                    $courses = array_slice($courses, 0, 3);
-
-                    // Add the courses to the recommendations array
+                    
                     foreach ($courses as $course) {
-                        $recommendations[] = [
+                        // Add the course to the items array
+                        $items['courses'][] = [
                             'id' => $course->id,
+                            'category' => $course->category,
                             'fullname' => $course->get_formatted_fullname(),
                             'shortname' => $course->get_formatted_shortname(),
+                            'lang' => $course->lang,
+                            'summary' => '',
                             'timemodified' => userdate($course->timemodified, '%d %B %Y'),
-                            'category' => $category->get_formatted_name(),
                             'url' => (new moodle_url('/course/view.php', ['id' => $course->id]))->out(),
                             'image' => (new moodle_url('/theme/edash/pix/category.jpg'))->out(),
                         ];
                     }
                 }
-
             }
         }
 
-        // TODO: Send a request to the recommendation engine to get the recommendations
+        return $items;
+    }
 
-        // TODO: Validate the recommendations using context
+    /**
+     * Helper function to send a request to the recommendation engine
+     * @param int $user_id the id of the user
+     * @param string $institution the institution of the user
+     * @param string $function the function of the user
+     * @param array $items the categories and courses
+     * @return array the recommendations
+     */
+    private static function send_request($user_id, $institution, $function, $items)
+    {
+        // FastAPI endpoint
+        $my_api_url = 'http://127.0.0.1:8000/recommendations';
+
+        // Create the data array
+        $data = [
+            'user_id' => $user_id,
+            'institution' => $institution,
+            'function' => $function,
+            'course_data' => $items
+        ];
+
+        // Encode the data to JSON
+        $json_data = json_encode($data);
+
+        // Create a new cURL resource
+        $ch = curl_init($my_api_url);
+
+        // Set the cURL options
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json'
+        ]);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+
+        // Execute the cURL request
+        $response = curl_exec($ch);
+
+        // Check for cURL errors
+        if ($response === false) {
+            throw new \Exception('cURL Error: ' . curl_error($ch));
+        }
+
+        // Decode the JSON response
+        $response = json_decode($response, true);
+        $recommendations = $response['recommendations'];
+
+        // Close the cURL resource
+        curl_close($ch);
 
         return $recommendations;
     }
